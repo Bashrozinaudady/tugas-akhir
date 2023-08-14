833990ae13f9cdd35ac99e8357d49a74567cc7c6
Bashrozinaudady <bashrozinaudady@gmail.com>
2023-08-14 11:54:17 +0700

    update

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -71,7 +71,7 @@
                                         novalidate>
                                         @csrf
                                         <div class="col-12">
-                                            <label for="yourUsername" class="form-label">Usernsme</label>
+                                            <label for="yourUsername" class="form-label">Username</label>
                                             <div class="input-group has-validation">
                                                 <input type="text" name="email" class="form-control"
                                                     id="yourUsername" required>
@@ -95,10 +95,6 @@
                                         </div>
                                         <div class="col-12">
                                             <button class="btn btn-primary w-100" type="submit">Masuk</button>
-                                        </div>
-                                        <div class="col-12">
-                                            <p class="small mb-0">Don't have account? <a
-                                                    href="pages-register.html">Create an account</a></p>
                                         </div>
                                     </form>
 
